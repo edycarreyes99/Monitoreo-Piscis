f90dbd8587a0319957a61d8b50f97e14e851b238
Edycar Reyes <edycarreyes@gmail.com>
2018-02-23 10:29:19 -0600

    se crea el observador en firebaselogin.js para determinar usuarios

--- js/firebaselogin.js
+++ js/firebaselogin.js
@@ -9,17 +9,38 @@ function registrar(){
         console.log(errorMessage);
         // ...
       });
+}
 
-      function ingresa(){
-          var email = document.getElementById('emailingreso').value;
-          var contrasena = document.getElementById('contrasenaingreso').value;
-          firebase.auth().signInWithEmailAndPassword(email, contrasena).catch(function(error) {
-            // Handle Errors here.
-            var errorCode = error.code;
-            var errorMessage = error.message;
-            console.log(errorcode);
-            console.log(errorMessage);
-            // ...
-          });
-      }
+function ingresa(){
+    var email = document.getElementById('emailingreso').value;
+    var contrasena = document.getElementById('contrasenaingreso').value;
+    firebase.auth().signInWithEmailAndPassword(email, contrasena).catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      console.log(errorcode);
+      console.log(errorMessage);
+      // ...
+    });
+}
+
+function observador(){
+    firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+            console.log('Hay Usuarios Activos');
+          // User is signed in.
+          var displayName = user.displayName;
+          var email = user.email;
+          var emailVerified = user.emailVerified;
+          var photoURL = user.photoURL;
+          var isAnonymous = user.isAnonymous;
+          var uid = user.uid;
+          var providerData = user.providerData;
+          // ...
+        } else {
+          // User is signed out.
+          console.log('No hay Ningun Usuario Activo');
+          // ...
+        }
+      });
 }
