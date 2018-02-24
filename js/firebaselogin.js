f689436206279c6d3a3544af0ef9a999a68ef7f3
Edycar Reyes <edycarreyes@gmail.com>
2018-02-23 19:59:30 -0600

    Se agregan archivos y se modifican otros
    se agregan las librerias de bootstrap en servidores externos y se termina la configuracion de inicios y registros con confirmacion de inicios de sesion

--- js/firebaselogin.js
+++ js/firebaselogin.js
@@ -1,7 +1,12 @@
 function registrar(){
     var emailregistro = document.getElementById('emailregistro').value;
     var contrasenaregistro = document.getElementById('contrasenaregistro').value;
-    firebase.auth().createUserWithEmailAndPassword(emailregistro, contrasenaregistro).catch(function(error) {
+    firebase.auth().createUserWithEmailAndPassword(emailregistro, contrasenaregistro)
+    .then(function(){
+        verificaUsuario()
+    })
+
+    .catch(function(error) {
         // Handle Errors here.
         var errorCode = error.code;
         var errorMessage = error.message;
@@ -14,7 +19,8 @@ function registrar(){
 function ingresa(){
     var emailingresa = document.getElementById('emailingreso').value;
     var contrasenaingresa = document.getElementById('contrasenaingreso').value;
-    firebase.auth().signInWithEmailAndPassword(emailingresa, contrasenaingresa).catch(function(error) {
+    firebase.auth().signInWithEmailAndPassword(emailingresa, contrasenaingresa)
+    .catch(function(error) {
       // Handle Errors here.
       var errorCode = error.code;
       var errorMessage = error.message;
@@ -28,10 +34,14 @@ function observador(){
     firebase.auth().onAuthStateChanged(function(user) {
         if (user) {
             console.log('Hay Usuarios Activos');
-            contenido();
+            contenido(user);
           // User is signed in.
           var displayName = user.displayName;
           var email = user.email;
+          console.log('******************')
+          console.log(user.emailVerified)
+          console.log('******************')
+
           var emailVerified = user.emailVerified;
           var photoURL = user.photoURL;
           var isAnonymous = user.isAnonymous;
@@ -48,15 +58,35 @@ function observador(){
 
 observador();
 
-function contenido(){
+function contenido(user){
+    var user = user;
     var content = document.getElementById('contenido');
-    content.innerHTML = `
-    <p>Bienvenido!</p>
-    <button onclick="cerrar()">Cerrar Sesion</button>
-    `;
+    if(user.emailVerified)
+    {
+        content.innerHTML = `
+        <p>Bienvenido!</p>
+        <button onclick="cerrar()">Cerrar Sesion</button>
+        `;
+    }else{
+        content.innerHTML=`
+        <h1>Para poder iniciar sesion primero tiene que verificar su correo electronico<h1>
+        `;
+    }
 }
 
 function cerrar(){
     firebase.auth().signOut().then(function(){
     console.log('Sesion Cerrada')}).catch(function(error){console.log(error)})
+}
+
+function verificaUsuario(){
+    var user = firebase.auth().currentUser;
+
+    user.sendEmailVerification().then(function() {
+    // Email sent.
+    console.log('enviando');
+    }).catch(function(error) {
+    // An error happened.
+    console.logo(error);
+    });
 }
