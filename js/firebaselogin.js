6df6c163ce258058a5e219523bdb52a2445a7c82
Edycar Reyes <edycarreyes@gmail.com>
2018-02-28 12:01:07 -0600

    se hacen cambios en las dependencias

--- js/firebaselogin.js
+++ js/firebaselogin.js
@@ -63,7 +63,6 @@ function observador(){
           console.log(user.emailVerified)
           console.log(user.displayName);
           console.log('******************')
-
           var emailVerified = user.emailVerified;
           var photoURL = user.photoURL;
           var isAnonymous = user.isAnonymous;
@@ -100,6 +99,7 @@ function contenido(user){
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
         `;
+        window.location.href = "pages/temperatura.html"
     }else{
         content.innerHTML=`
         <p class="h4">Para poder iniciar sesion primero tiene que verificar su correo electronico<p>
